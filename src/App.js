80a2124e00cbd2643d593e21e3b200856340e863
oren amema <orenamema@gmail.com>
2020-02-10 08:32:42 -0800

    adding gif

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import DatatablePage from "./components/DatatablePage";
 function App() {
   return (
     <div className="App">
+
 <div className="Header">
-  <h1>Employee Directory</h1>
+  <h1>Employee Directory </h1>
+  <h8>Click on carrots to filter by heading or use the search box to narrow your results</h8>
 </div>
         <DatatablePage/>
 
